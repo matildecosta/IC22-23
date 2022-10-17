c9ce54c0e81fc7862cf9027dfffc33eee03ce11e
matilde_costa <matilde.a.costa@ua.pt>
2022-10-17 14:44:08 +0100

    bitstream test

--- BitStream_test.c
+++ BitStream_test.c
@@ -5,3 +5,50 @@
 
 using namespace std;
 
+int main(int argc, char* argv[]){
+    string mode {"r"};
+
+    if (argc < 2){
+        cerr << "test_bs [-r (def)] \n";
+        cerr << "        [-w] \n";
+        cerr << "        text_file bin_file \n";
+        return 1;
+    }
+
+    for (int n = 0; n < argc ; n++){
+        if(string(argv[n]) == "-r"){    // Reading mode
+            mode = "r";
+            break;
+        }
+    }
+
+    for (int n = 0; n < argc ; n++){
+        if(string(argv[n]) == "-w"){    // Writing mode
+            mode = "w";
+            break;
+        }
+    }
+
+
+    BitStream bs {argv[argc-1], mode};
+
+    if (mode == "r"){
+        ofstream ofs {argv[argc-2]};
+        int bit;
+        
+        while ((bit = bs.read_bit()) != EOF){
+            ofs << bit << endl;
+        }
+        ofs.close();
+    } else {
+        ifstream ifs {argv[argc-2]};
+        int val;
+        while (ifs >> val)
+        {
+            bs.write_bit(val);
+        }
+        ifs.close();
+    }
+
+    return 0;
+}
