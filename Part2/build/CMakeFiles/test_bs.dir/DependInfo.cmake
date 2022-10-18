ed1c5b40df6416221c98a1d5b882143b95296764
matilde_costa <matilde.a.costa@ua.pt>
2022-10-18 20:44:05 +0100

    last

---
+++ Part2/build/CMakeFiles/test_bs.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/matilde/Desktop/IC/Lab1/Part2/test_bs.cpp" "CMakeFiles/test_bs.dir/test_bs.cpp.o" "gcc" "CMakeFiles/test_bs.dir/test_bs.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Part2/build/CMakeFiles/test_bs.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/matilde/Desktop/IC/Lab1/test_bs"
+  "/home/matilde/Desktop/IC/Lab1/test_bs.pdb"
+  "CMakeFiles/test_bs.dir/test_bs.cpp.o"
+  "CMakeFiles/test_bs.dir/test_bs.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_bs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
