203b4e29f2754f730282d0dab9ca93beb55f1fd9
matilde_costa <matilde.a.costa@ua.pt>
2022-10-18 22:31:57 +0100

    last

---
+++ build/CMakeFiles/bitstr.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/matilde/Desktop/IC/Lab1;/build/sndfile-example-bin/bitstr"
+  "/home/matilde/Desktop/IC/Lab1;/build/sndfile-example-bin/bitstr.pdb"
+  "CMakeFiles/bitstr.dir/test_bs.cpp.o"
+  "CMakeFiles/bitstr.dir/test_bs.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bitstr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/bitstr.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for bitstr.
+# This may be replaced when dependencies are built.
