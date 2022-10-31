03b8016c284d7db3dfe9cdb2f3f980c5d7151af6
matilde_costa <matilde.a.costa@ua.pt>
2022-10-31 18:48:45 +0000

    ex1 done

--- Lab2/copy.cpp
+++ Lab2/copy.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
 
 
 using namespace std;
@@ -9,16 +10,13 @@ using namespace cv;
 int main (int argc, char** argv)
 {
     Mat image;
-    image = imread(argv[argc-2], CV_LOAD_IMAGE_COLOR);
+    image = imread(argv[argc-2], IMREAD_COLOR );
 
-    if (!image.data)
-    {
+    if (!image.data){
         cout << "Could not open or find the image" << endl;
         return -1;
     }
-    
     imwrite(argv[argc-1], image);
-
     imshow("Display window", image);
     waitKey(0);
     return 0;
