1f760699bc0f77d077313245775d6e958a343cfa
matilde_costa <matilde.a.costa@ua.pt>
2022-12-30 13:06:49 +0000

    readme updated

---
+++ Lab3/build/CMakeFiles/findlang.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../text_analysis/findlang"
+  "../text_analysis/findlang.pdb"
+  "CMakeFiles/findlang.dir/findlang.cpp.o"
+  "CMakeFiles/findlang.dir/findlang.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/findlang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab3/build/CMakeFiles/locatelang.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../text_analysis/locatelang"
+  "../text_analysis/locatelang.pdb"
+  "CMakeFiles/locatelang.dir/locatelang.cpp.o"
+  "CMakeFiles/locatelang.dir/locatelang.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/locatelang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
